c45c8c1ee3a4a58a90826b7a8763f54c5199e957
shenshiKS <3230314076@qq.com>
2025-07-19 14:21:45 +0800

    Add files via upload

--- 基础.lua
+++ 基础.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/shenshiKS/king/refs/heads/main/114514.lua", true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FengY4/FengY4/refs/heads/main/Fengui.lua", true))()
 ----------------------------------------------------------------------------------------------------------------------------------------UI
 local window = library:new("殺脚本(测试版)")--脚本名字
 ----------------------------------------------------------------------------------------------------------------------------------------
@@ -6,8 +6,8 @@ local window = library:new("殺脚本(测试版)")--脚本名字
 
 local creds = window:Tab("通用",'6031097229')--文件夹
 
-local credits = creds:section("通用",true)--文件
+local credits = creds:section("功能",true)--文件
 
-credits:Button("通用",function()
+credits:Button("功能",function()
     game:GetService("StarterGui"):SetCore("SendNotification", { 	Title = "Limb Reanim V2";	Text = "你被骗了";	Icon = "rbxthumb://type=Asset&id=13775452736&w=150&h=150"})Duration = 15;
 end)--功能
